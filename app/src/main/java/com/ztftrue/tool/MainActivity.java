7e61bfe2bbd1f557a0a84f9813b4e119e75c27a5
ztftrue <ztftrue@gmail.com>
2021-06-06 21:18:39 +0800

    add check permission

--- app/src/main/java/com/ztftrue/tool/MainActivity.java
+++ app/src/main/java/com/ztftrue/tool/MainActivity.java
@@ -1,8 +1,23 @@
 package com.ztftrue.tool;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.sudo) {
+            checkSudo();
+        } else if (view.getId() == R.id.setting) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
+    public void checkSudo() {
+        Observable.create((ObservableOnSubscribe<Boolean>) emitter -> {
+            if (!emitter.isDisposed()) {
+                String a = SystemUtils.startCommand("ls");
+                emitter.onNext(!TextUtils.isEmpty(a));
+                emitter.onComplete();
+            }
+        }).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<Boolean>() {
+
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Boolean o) {
+                if (o) {
+                    Toast.makeText(MainActivity.this, "Get sudo granted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "No sudo granted", Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        });
     }
 }
